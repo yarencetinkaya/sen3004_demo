4d9210def076184cb54462f60dbe8fcd21fadd79
yarenn <118934772+yarencetinkaya@users.noreply.github.com>
2025-04-03 10:10:43 +0300

    Add files via upload

---
+++ src/main/java/com/example/demo/Sen3004DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sen3004DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sen3004DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/Sen3004DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sen3004DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
